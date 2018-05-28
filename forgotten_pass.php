b2850f19f597a52a91bf7ab51040d5818378c11b
Nana Baah Akuffu <nanaakuffu@gmail.com>
2018-05-28 09:47:53 +0000

    Streamlined the forgotten password script

--- forgotten_pass.php
+++ forgotten_pass.php
@@ -23,71 +23,73 @@
     }
 ?>
 <nav class='navbar navbar-inverse navbar-fixed-top' role='navigation'>
-  <div class='container topstart'>
+  <div class='container'>
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class='navbar-header'>
-        <button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1'>
+        <!-- <button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1'>
             <span class='sr-only'>Toggle navigation</span>
             <span class='icon-bar'></span>
             <span class='icon-bar'></span>
             <span class='icon-bar'></span>
-        </button>
+        </button> -->
         <a class='navbar-brand bitterlabel' href='#'> <i class='fa fa-home fa-fw'></i> Finishers Co. Ltd </a>
     </div>
   </div>
 </nav>
-<div class="container-fluid">
-  <div class="container">
-    <!-- <div class="jumbotron">
-      <h1> Welcome! </h1>
-      <p> It appears you are having problems in logging in.
-          You only need to fill the form below to retrieve your password.
-          This is just to identify you. </p>
-    </div> -->
 
-    <div class='row' style='margin-top:-50px'>
-        <div class='col-md-3'>
-          <br />
-        </div>
-        <div class='col-md-6'>
-          <div class='panel panel-default' style='margin-top:90px'>
-            <div class='panel-body'>
-                <form action='verify_answer.php' id='reset' method='POST'>
-                  <div class='form-group'>
-                      <label for='user_name'> Username: </label>
-                      <input type='text' class='form-control' id='user_name' name='user_name'
-                        value='<?php $user = (isset($_GET['use'])) ? $username : $_POST['user_name']; echo $user; ?>' placeholder='Enter username' readonly>
-                  </div>
-                  <div class='form-group'>
-                      <label for='user_name'> Security Question: </label>
-                      <input type='text' class='form-control' id='sec_ques' name='security_question'
-                        value='<?php $sec_q = (isset($_GET['use'])) ? $sec_ques : $_POST['security_question']; echo $sec_q; ?>' placeholder='Security Question' readonly>
-                  </div>
-                  <div class='form-group'>
-                      <label for='security_answer'> Answer: </label>
-                      <input type='text' class='form-control' id='answer' name='answer'
-                        placeholder='Answer' required>
-                  </div>
-                  <button class='btn btn-primary' type='submit' name='verify' form='reset'
-                          value='retrieve'><i class='fa fa-check-square-o'></i> Verify Answer </button>
-                </form>
-            </div>
+<div class="container">
+  <!-- <div class="jumbotron">
+    <h1> Welcome! </h1>
+    <p> It appears you are having problems logging in.
+        You only need to answer the question o retrieve your password.
+        This is just to identify you. </p>
+  </div> -->
+
+  <div class='row'>
+      <div class='col-md-3'>
+        <br />
+      </div>
+      <div class='col-md-6'>
+        <div class='panel panel-default' style='margin-top:90px'>
+          <div class="panel-heading">
+            Answer this question to <b>Retrieve Password</b>
           </div>
-            <?php
-              if (isset($_SESSION['message'])) {
-                echo "<div class='alert alert-danger'>
-                          ", $_SESSION['message'],
-                     "</div>";
-                unset($_SESSION['message']);
-              }
-            ?>
+          <div class='panel-body'>
+              <form action='verify_answer.php' id='reset' method='POST'>
+                <div class='form-group'>
+                    <label for='user_name'> Username: </label>
+                    <input type='text' class='form-control' id='user_name' name='user_name'
+                      value='<?php $user = (isset($_GET['use'])) ? $username : $_POST['user_name']; echo $user; ?>' placeholder='Enter username' readonly>
+                </div>
+                <div class='form-group'>
+                    <label for='user_name'> Security Question: </label>
+                    <input type='text' class='form-control' id='sec_ques' name='security_question'
+                      value='<?php $sec_q = (isset($_GET['use'])) ? $sec_ques : $_POST['security_question']; echo $sec_q; ?>' placeholder='Security Question' readonly>
+                </div>
+                <div class='form-group'>
+                    <label for='security_answer'> Answer: </label>
+                    <input type='text' class='form-control' id='answer' name='answer'
+                      placeholder='Answer' required>
+                </div>
+                <button class='btn btn-primary' type='submit' name='verify' form='reset'
+                        value='retrieve'><i class='fa fa-check-square-o'></i> Verify Answer </button>
+              </form>
+          </div>
         </div>
-        <div class='col-md-3'>
-          <br />
-        </div>
-    </div>
-    <hr>
+          <?php
+            if (isset($_SESSION['message'])) {
+              echo "<div class='alert alert-danger'>
+                        ", $_SESSION['message'],
+                   "</div>";
+              unset($_SESSION['message']);
+            }
+          ?>
+      </div>
+      <div class='col-md-3'>
+        <br />
+      </div>
   </div>
+  <hr>
 </div>
 
 <?php
